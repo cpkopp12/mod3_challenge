4ef8ddc9f1e90a5974f22174c8b152edf45f0565
cpkopp <cameronpkopp@gmail.com>
2022-08-14 12:03:43 -0400

    userInput obj

--- friendly-parakeet-main/Develop/script.js
+++ friendly-parakeet-main/Develop/script.js
@@ -1,10 +1,34 @@
 // Assignment code here
   
-  //generatePassword() function
+  
 
+var userInput = {
+  numChar = ;
+  lowerCaseChar = ;
+  upperCaseChar = ;
+  numericChar = ;
+  specialChar = ;
+}
+
+
+function generatePassword() {
+    
     //get parameters from user
-
+    var length = window.prompt("Enter password length (integer [8-128])");
+    var lowerCaseChar = window.confirm("Would you like to include lowercase characters? (Must select OK for atleast one of the four character types)");
+    var upperCaseChar = window.confirm("Would you like to include uppercase characters? (Must select OK for atleast one of the four character types)");
+    var numericChar = window.confirm("Would you like to include numeric characters? (Must select OK for atleast one of the four character types)");
+    var specialChar = window.confirm("Would you like to include numeric characters? (Must select OK for atleast one of the four character types)");
+    
+    console.log(length);
+    console.log(lowerCaseChar);
+    console.log(upperCaseChar);
+    console.log(numericChar);
+    console.log(specialChar);
     //randomize according to user input parameters
+
+
+  }
 
 
 
